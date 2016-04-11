f8cb99a5cd4e960164176b18db93420de2d71721
fenixwu <fenixwu.work@gmail.com>
2016-04-11 17:55:18 +0800

    Remove useless method in counter_d

--- counter_d/app/js/store/configureStore.js
+++ counter_d/app/js/store/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import logger from 'redux-logger';
 import Reducers from 'reducers/index';
 
